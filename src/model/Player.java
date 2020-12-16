2e1a67af3dd9e6171dd699740cd9be40b87dca56
FeuFeve <clement-potin@outlook.com>
2020-12-16 22:22:18 +0100

    Add methods to calculate player attack/defense/hp bonuses

--- src/model/Player.java
+++ src/model/Player.java
@@ -22,7 +22,23 @@ public class Player extends GameObject {
         this.name = name;
     }
 
+    public void debugModifyStats(int weapons, int shields, int dome, int nest) {
+        attackMultiplier = defenseMultiplier = 1 + weapons * 0.1;
+        hpMultiplier = 1 + shields * 0.1;
+        domeHpMultiplier = 1.1 + 0.05 * dome;
+        nestHpMultiplier = 1.3 + 0.15 * nest;
+    }
+
     public void recalculateLayingSpeedMultiplier() {
         layingSpeedMultiplier = Math.pow(0.9, layingSpeedLevel);
+    }
+
+    public double getHpMultiplierFor(String location) {
+        switch (location) {
+            case "hunting field": return hpMultiplier;
+            case "dome": return hpMultiplier + domeHpMultiplier - 1;
+            case "nest": return hpMultiplier + nestHpMultiplier - 1;
+            default: return 0;
+        }
     }
 }
