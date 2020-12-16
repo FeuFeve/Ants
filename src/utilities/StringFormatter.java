3b72015e9ccabdc3abd05d9fd7ce90d82b99dbf2
FeuFeve <clement-potin@outlook.com>
2020-12-16 22:18:46 +0100

    Add a method to format big numbers

--- src/utilities/StringFormatter.java
+++ src/utilities/StringFormatter.java
@@ -1,6 +1,19 @@
 package utilities;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class StringFormatter {
+
+    public static String bigNumber(long number) {
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
+        symbols.setGroupingSeparator(' ');
+        formatter.setDecimalFormatSymbols(symbols);
+        return formatter.format(number);
+    }
 
     public static String firstLeftSecondRightAlign(int totalLength, String first, String second, boolean sideSpace) {
         if (first.length() + second.length() >= totalLength)
