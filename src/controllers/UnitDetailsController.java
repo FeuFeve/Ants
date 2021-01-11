c33a147f47cd5e86d4f36a883b4e3805a6aafe6c
FeuFeve <clement-potin@outlook.com>
2021-01-11 18:36:49 +0100

    Add life, attack and defense icons on the Queen page

--- src/controllers/UnitDetailsController.java
+++ src/controllers/UnitDetailsController.java
@@ -10,6 +10,9 @@ public class UnitDetailsController {
 
     @FXML ImageView unitImage;
 
+    @FXML ImageView hpImageView;
+    @FXML ImageView attackImageView;
+    @FXML ImageView defenseImageView;
     @FXML Label hpLabel;
     @FXML Label attackLabel;
     @FXML Label defenseLabel;
@@ -23,19 +26,28 @@ public class UnitDetailsController {
 
     public void setIsWorker() {
         this.unit = null;
+
         unitImage.setImage(GameImage.getWorkerImage());
+
+        hpImageView.setVisible(false);
+        attackImageView.setVisible(false);
+        defenseImageView.setVisible(false);
         hpLabel.setText("");
         attackLabel.setText("");
         defenseLabel.setText("");
+
         nameLabel.setText("Worker");
     }
 
     public void setUnit(Unit unit) {
         this.unit = unit;
+
         unitImage.setImage(GameImage.getUnitImage(unit));
+
         hpLabel.setText(String.valueOf(unit.hp));
         attackLabel.setText(String.valueOf(unit.attack));
         defenseLabel.setText(String.valueOf(unit.defense));
+        
         nameLabel.setText(unit.name);
     }
 }
